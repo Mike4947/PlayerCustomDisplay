603b1f09d4d7415618a47afccc400cbf7a925c74
Mike4947 <mleblond10@hotmail.com>
2025-06-13 21:25:35 -0400

    rewinded to v0.0.2 because update messages didn't work

--- src/main/java/com/example/pcd/main.java
+++ src/main/java/com/example/pcd/main.java
@@ -5,30 +5,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class main extends JavaPlugin {
 
     private int customPlayerCount = -1;
-    private UpdateChecker updateChecker; // Add a field for the update checker
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         loadPlayerCount();
 
-        // --- NEW: Initialize and run the UpdateChecker ---
-        this.updateChecker = new UpdateChecker(this);
-        // Check for updates 5 seconds after the server starts to avoid spam
-        getServer().getScheduler().runTaskLater(this, this.updateChecker::check, 100L);
-
+        // --- UPDATED COMMAND REGISTRATION ---
+        // We create one instance of our command class
         PCDCommand pcdCommand = new PCDCommand(this);
+        // We tell the server that this instance will handle BOTH execution and tab completion
         getCommand("pcd").setExecutor(pcdCommand);
         getCommand("pcd").setTabCompleter(pcdCommand);
 
         getServer().getPluginManager().registerEvents(new ServerListListener(this), this);
 
         getLogger().info("PlayerCustomDisplay has been enabled!");
-    }
-
-    // A "getter" for other classes to access the update checker
-    public UpdateChecker getUpdateChecker() {
-        return updateChecker;
     }
 
     public void loadPlayerCount() {
